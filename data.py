14813d70457f7e0126307d3c4faee230ee4694ec
Eddie Lee <el.trompetero@gmail.com>
2019-02-08 23:46:02 -0500

    added count_zeros_at_end() and split_at_zeros()

--- data.py
+++ data.py
@@ -8,7 +8,63 @@ import numpy as np
 import os
 from scipy.io import loadmat
 from scipy.interpolate import interp1d
+from numba import njit
 DATADR = os.path.expanduser('~')+'/Dropbox/Research/py_lib/data_sets/neuron_timme'
+
+
+@njit
+def count_zeros_at_end(x):
+    i = 0
+    while i<len(x) and x[-1-i]==0:
+        i += 1
+    return i
+
+def split_at_zeros(x, n_zeros):
+    """Split x at any sequence of at least n_zeros. And return subsets pruned such that no
+    returned sequence is sandwiched by zeros."""
+
+    assert n_zeros>=1
+    assert x.ndim==1
+    y = []
+    countingZeros = x[0]==0
+    done = False
+    i = 0
+    startix = 0
+    
+    # don't consider any initial zeros
+    while  i<x.size and x[i]==0:
+        i += 1
+        startix += 1
+
+    while not done:
+        if countingZeros:
+            print('zero')
+            zerocounter = 0
+            while i<x.size and x[i]==0:
+                zerocounter += 1
+                i += 1
+            if zerocounter>=n_zeros:
+                split = True 
+            else:
+                split = False
+            countingZeros = False
+        else:
+            print('not zero')
+            if split:
+                y.append(x[startix:i])
+                y[-1] = y[-1][:-count_zeros_at_end(y[-1])]
+                startix = i
+                split = False
+                
+            while i<x.size and x[i]:
+                i += 1
+            countingZeros = True
+
+        if i==x.size:
+            y.append(x[startix:i])
+            done = True
+
+    return y
 
 
 class NeuronData():
@@ -44,6 +100,51 @@ class NeuronData():
             sortedSpikeTimes.sort()
             self.interspikeInterval = np.diff(sortedSpikeTimes).mean()*self.binsize
         return self.interspikeInterval
+
+    def cat_avalanches(self, dt=None, min_len=10):
+        """
+        Cluster neuron spikes into avalanches given time bin discretization and using time
+        contiguous cascades.
+
+        Parameters
+        ----------
+        dt : float, None
+            If not specified, the average interspike interval from the data is used.
+        min_len : int, 10
+            Min number of contiguous bins for an avalanche to fire across to be included.
+
+        Returns
+        -------
+        list of ndarrays
+            Each ndarray specified how many different neurons were firing at each point in
+            time.
+        """
+        
+        if dt is None:
+            dt = self.calculate_interspike()
+        assert dt>=self.binsize, "Specified dt cannot be smaller than recording discreteness."
+
+        # round each spike timing to the nearest bin
+        spikes_ = self.spikes[:]
+        factor = dt/self.binsize
+        for i in range(self.n_neurons):
+            spikes_[i] = np.around(spikes_[i]/factor).astype(int)
+        assert all([s[-1]<1e7 for s in spikes_]), "Too large of an array formed."
+        
+        binnedCounts = np.zeros(1+max([s[-1] for s in spikes_]), dtype=int)
+        for i in range(self.n_neurons):
+            binnedCounts[spikes_[i]] += 1
+        
+        # identify contiguous sections where at least one neuron is firing in every time bin
+        avalanches = np.split(binnedCounts, np.where(np.diff(np.cumsum(binnedCounts==0))>cutoff)[0]+1)
+        avalanches = [a for a in avalanches if len(a)>min_len]
+        # remove 0 if it's the first entry
+        avalanches = [a[1:] if a[0]==0 else a for a in avalanches]
+
+        # cache avalanches
+        self._avalanches = avalanches
+
+        return avalanches
 
     def avalanches(self, dt=None, min_len=10):
         """
